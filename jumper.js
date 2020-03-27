559c8303fc2bc558714bd77f48a4efab5b360ac6
jwbraith <jwbraith@gmail.com>
2020-03-26 20:58:21 -0700

    Switched everything to a bouncing ball p5js sketch

---
+++ jumper.js
@@ -0,0 +1,129 @@
+class Jumper {
+    constructor(x, y) {
+        this.pos = createVector(x, y);
+        this.vel = p5.Vector.random2D();
+        // this.vel.setMag(0.3);
+        this.acc = createVector(0, 0);
+        this.radius = 12;
+        this.lifespan = 120;
+        this.maxspeed = 12;
+        this.maxforce = 0.5;
+        this.lerpAmt = 0.0;
+    }
+
+    aim() {
+        let propulsion = new createVector();
+        //pick polar coordinates;
+
+        if (this.isAtRest()) {
+        let theta = random(-PI);
+        let magnitude = random(23,33);
+        
+        propulsion.x = magnitude * sin(theta);
+        propulsion.y = magnitude * cos(theta);
+        
+        }
+        return propulsion;
+    }
+
+    isAtRest() {
+        let isAtRest = false;
+        if (this.vel.x > -0.1 && this.vel.x < 0.1 && this.vel.y > -0.1 && this.vel.y < 0.1) {
+            isAtRest = true;
+        }
+        return isAtRest;
+    }
+
+
+
+    friction() {
+        let diff = height - (this.pos.y + this.radius);
+        if (diff < 1) {
+            // console.log('friction');
+            //Get the direction of friction
+            let friction = this.vel.copy();
+            friction.normalize();
+            friction.mult(-1);
+            
+            //Get the magnitude of friction
+            let mu = 0.05;
+            // let normal = this.mass;
+            friction.setMag(mu);
+
+            this.applyForce(friction);
+        }
+    }
+
+    update() {
+        //update acceleration
+        this.vel.add(this.acc);
+        // limit speed
+        this.vel.limit(this.maxspeed);
+        this.pos.add(this.vel);
+        // reset acceleration to zero each cycle
+        this.acc.mult(0);
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    seek(target) {
+        // desired is a vector pointing from position to the target
+        let desired; 
+        desired = p5.Vector.sub(target, this.pos);
+        // Scale to maxspeed
+        desired.setMag(this.maxspeed);
+        // Steering = desired minus velocity
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce); // limit to maximum steering force
+
+        // return steer;
+        this.applyForce(steer);
+    }
+
+    eat(list) {
+        let record = Infinity;
+        let closestIndex = -1;
+        for (let i = 0; i < list.length; i++) {
+            // let d = dist(this.pos.x, this.pos.y, list[i].pos.x, list[i].pos.y);
+            let d = createVector(); 
+            d = this.pos.dist(list[i].pos);
+            if (d < record) {
+                record = d;
+                closestIndex = i;
+            }
+        }
+        if (record < 10) {
+            list.splice(closestIndex, 1);
+        } else if (closestIndex > -1) {
+        this.seek(list[closestIndex].pos);
+        }
+    }
+
+    edges() {
+        let bounce = -0.9;
+        if (this.pos.x < this.radius) {
+            this.pos.x = this.radius;
+            this.vel.x *= bounce;
+        }
+        if (this.pos.x > width - this.radius) {
+            this.pos.x = width - this.radius; 
+            this.vel.x *= bounce;
+        }
+        if (this.pos.y > height - this.radius) {
+            this.pos.y = height - this.radius;
+            this.vel.y *= bounce;
+        }
+    }
+
+    show() {
+        //draw a circle
+        push();
+        translate(this.pos.x, this.pos.y);
+        noStroke();
+        fill(255, 255, 255, this.lifespan);
+        ellipse(0, 0, this.radius * 2);
+        pop();
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,104 +1,19 @@
-//the text container for the number
-//the button variable
-//the number to put in the text container
-//the number to put in the rate text container
-//the number that auto increases the number of chopped trees
-
-let choppedTrees;
-let axe;
-let firstVal = 0.0;
-let treeRate;
-let chopRate = 0.0;
-
-//the text container for the beavers number
-//the button variable for buying a new beaver
-//the number to put in the text container
-//the number for the rate container
-//the number that auto increases the number of beavers
-
-let numBeavers;
-let buy;
-let secondVal = 0;
-let birthRate;
-let breedRate = 0.0;
-
-//the text container for the number of beaver breeders
-//the button variable for hiring a new beaver breeder
-//the number to put in the text container
-
-let numBreeders, hire, thirdVal = 0;
-
-//var for the hidden table row containing breeders
-//allows it to be revealed by meeting a condition
-let thirdRow;
-
+let jumper;
 
 function setup() {
-	// createCanvas(500, 500);
-	frameRate(20);
-	//assign selected ids to each text container
-	choppedTrees = select('#numTrees');
-	numBeavers = select('#beavers');
-	numBreeders = select('#breeders');
-	treeRate = select('#treeRate');
-	birthRate = select('#birthRate');
-
-	//assign buttons by selected id
-	axe = select('#axe');
-	buy = select('#buy');
-	hire = select('#hire');
-
-	//tie functions to each clickable button
-	axe.mousePressed(increaseVal);
-	buy.mousePressed(buyBeav);
-	hire.mousePressed(hireBreeder);
-
-	thirdRow = select('#thirdRow');
-	thirdRow.style('visibility:hidden');
+	createCanvas(600, 600);
+	ellipseMode(CENTER);
+	jumper = new Jumper(width / 2, height / 2);
 }
-
-function increaseVal() {
-	firstVal++;
-}
-
-function buyBeav() {
-	if (firstVal >= 2) {
-		secondVal += 1;
-		chopUpdate();
-		firstVal -= 2;
-	}
-}
-
-function chopUpdate() {
-	chopRate = secondVal * 0.002;
-}
-
-function hireBreeder() {
-	if (secondVal >= 4) {
-		thirdVal += 1;
-		breedRate = thirdVal * 0.001;
-		secondVal -= 4;
-		chopUpdate();
-	}
-}
-
-
 
 function draw() {
-	choppedTrees.html(firstVal.toFixed(2));
-	numBeavers.html(secondVal.toFixed(2));
-	numBreeders.html(thirdVal.toFixed(2));
-	treeRate.html(chopRate.toFixed(3));
-	birthRate.html(breedRate.toFixed(3));
-	if (secondVal > 0) {
-		firstVal += chopRate;
-	}
-	if (thirdVal > 0) {
-		secondVal += breedRate;
-		chopUpdate();
-	}
-	if (firstVal > 5) {
-		thirdRow.style('visibility:visible');
-	}
+	background(0,5);
+	let gravity = createVector(0, 0.2);
+	jumper.applyForce(gravity);
 
+		jumper.applyForce(jumper.aim());
+	jumper.friction();
+	jumper.edges()
+	jumper.update();
+	jumper.show();
 }
