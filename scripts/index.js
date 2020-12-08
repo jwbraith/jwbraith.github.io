adae9d67ee5e1e07f2d30d2e7cbfa0c809abb366
John Braithwaite <jwbraith@gmail.com>
2020-12-07 20:19:36 -0800

    adopting a quadtree tks shiffman

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-
+let qt;
 let particles = [];
 let numberOfParticles = 100;
 
@@ -8,12 +8,29 @@ function setup() {
     particles.push(new Particle(30, 30, 8));
   }
   canvas.parent('sketch-holder')
+  let boundary = new Rectangle(200, 200, 200, 200);
+  qt = new QuadTree(boundary, 4);
+  console.log(qt);
+
+  // for (let i = 0; i < 50; i++) {
+  //   let p = new Point(random(width), random(height));
+  //   qt.insert(p);
+  // }
 }
 
 function draw() {
-  background(0);
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].update();
-    particles[i].show();
+  if (mouseIsPressed) {
+    let m = new Point(mouseX, mouseY);
+    qt.insert(m);
   }
+  background(0);
+  qt.show();
 }
+
+// function draw() {
+//   background(0);
+//   for (let i = 0; i < particles.length; i++) {
+//     particles[i].update();
+//     particles[i].show();
+//   }
+// }
