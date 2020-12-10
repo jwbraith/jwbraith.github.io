adfad8031f42d9b9974fef717590f50b7e337432
John Braithwaite <jwbraith@gmail.com>
2020-12-10 01:46:23 -0800

    addressed mobile concerns

--- scripts/particle.js
+++ scripts/particle.js
@@ -1,7 +1,7 @@
 class Particle {
   constructor(x, y, r) {
     this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D().mult(random(0.2, 1));
+    this.vel = p5.Vector.random2D().mult(random(0.05, 0.5));
     this.radius = r;
     this.highlight = false;
   }
